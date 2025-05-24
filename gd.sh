09f77fe03f7ea0ceba5e4d08c1f8747622537b5f
dddwahs <dddwah11@gmail.com>
2025-05-24 16:20:52 +0800

    Site updated: 2025-05-24 16:20:51

--- gd.sh
+++ gd.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-hexo clean
  echo '执行clean';
+hexo clean
+ echo '执行clean完成';
 
+ echo '执行静态文件生成';
 hexo g
- echo '执行静态生成';
+ echo '执行静态生成完成';
 
-hexo d
  echo '部署到githubpage';
+hexo d
+ echo '部署到githubpage完成';
 
 
